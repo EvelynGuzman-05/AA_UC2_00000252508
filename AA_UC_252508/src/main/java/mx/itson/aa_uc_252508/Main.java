92a3b5b8358262e9ab1367628c7b9c008a1bfdcd
Evelyn Guzman <evelin21guzace@gmail.com>
2025-02-26 18:23:31 -0700

    Primer commit

---
+++ AA_UC_252508/src/main/java/mx/itson/aa_uc_252508/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.aa_uc_252508;
+
+/**
+ *
+ * @author Evelyn Guzman
+ */
+public class Main {
+    
+}
